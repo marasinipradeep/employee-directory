8421ebf9839bf79bb720336075712e93afbd27ce
marasinipradeep <marasinipradeep@gmail.com>
2020-09-20 14:33:03 +0930

    #Working on search form

--- employee-directory/src/Components/SearchBar/SearchBar.js
+++ employee-directory/src/Components/SearchBar/SearchBar.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import "./SearchBar.css"
+import data from "../../data"
+import EmployeeTitles from "../EmployeeTitles/EmployeeTitles"
 
-export default function SearchBar() {
-    return (
-        <section className="filter-container">
-            <form className="filter-form">
-                <input placeholder="Input"></input>
-                <button type="submit">Submit</button>
-            </form>
-        </section>
-    )
+export default class SearchBar extends Component {
+
+    state = {
+        data: null
+    }
+
+    searchSpace = (event) => {
+        let keyword = event.target.value;
+        this.setState({
+            data: keyword
+        })
+    }
+    render() {
+        return (
+
+            <section className="filter-container">
+                <form className="filter-form">
+                    <input placeholder="Input" onChange={(e) => this.searchSpace(e)}></input>
+                    <button type="submit">Submit</button>
+                </form>
+            </section>
+
+
+        )
+    }
 }
