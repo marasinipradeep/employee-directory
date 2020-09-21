dcdfd90d224772660300b2d280ef2fe9ef9b67c4
marasinipradeep <marasinipradeep@gmail.com>
2020-09-21 09:48:38 +0930

    Created handle search function and moved searchBar inside employeeData

--- employee-directory/src/Components/EmployeeContainer/EmployeeContainer.js
+++ employee-directory/src/Components/EmployeeContainer/EmployeeContainer.js
@@ -2,14 +2,13 @@ import React from 'react'
 
 
 import Heading from "../Heading/Heading"
-import SearchBar from "../SearchBar/SearchBar"
+
 import EmployeeData from "../EmployeeData/EmployeeData"
 
 export default function EmployeeContainer() {
     return (
         <div>
             <Heading />
-            <SearchBar />
             <EmployeeData />
         </div>
     )
--- employee-directory/src/Components/EmployeeData/EmployeeData.js
+++ employee-directory/src/Components/EmployeeData/EmployeeData.js
@@ -3,6 +3,8 @@ import EmployeeTitles from "../EmployeeTitles/EmployeeTitles"
 import TableHeading from "../TableHeading/TableHeading"
 import data from "../../data"
 
+import SearchBar from "../SearchBar/SearchBar"
+
 export default class EmployeeData extends Component {
 
     //Setting this.state.data tot the data json array
@@ -10,11 +12,18 @@ export default class EmployeeData extends Component {
         data
     }
 
+    handleSearch=()=>{
+        console.log("pradeep")
+    }
+
     //Map over this.state.friends and render a EmployeeTitleComponent for each employeee object
     render() {
         console.log(this.state)
         return (
             <div>
+                 <SearchBar 
+                 data={this.state.data}
+                 handleSearch={this.handleSearch}/>
                 <TableHeading/>
                 {this.state.data.map(data=>(
                     <EmployeeTitles
@@ -25,8 +34,6 @@ export default class EmployeeData extends Component {
                     email={data.email}
                     birthday={data.DOB}
                     />
-                   
-                    
 
                 )
                 
