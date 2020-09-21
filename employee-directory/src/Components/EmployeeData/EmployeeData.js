d3b21245cbafc618884d361300cc96101e6d54b4
marasinipradeep <marasinipradeep@gmail.com>
2020-09-21 16:09:15 +0930

    #Added checkbox criteria for filter

--- employee-directory/src/Components/EmployeeData/EmployeeData.js
+++ employee-directory/src/Components/EmployeeData/EmployeeData.js
@@ -18,23 +18,24 @@ export default class EmployeeData extends Component {
 
 
     handleSearch = (event) => {
-
         this.setState({search:event.target.value})
     }
 
-    //Map over this.state.friends and render a EmployeeTitleComponent for each employeee object
-    render() {
+      render() {
 
         let filterName = this.state.data.filter((name) => {
-            console.log("filtername")
             return name.name.toLowerCase().indexOf(this.state.search.toLowerCase())!== -1;
         })
 
         return (
+
+           
             <div>
+                
                 <SearchBar
                     data={this.state.data}
-                    handleSearch={this.handleSearch.bind(this)} />
+                    handleSearch={this.handleSearch.bind(this)} 
+                    />
                 <TableHeading />
                 {filterName.map(data => (
                     <EmployeeTitles
@@ -48,6 +49,7 @@ export default class EmployeeData extends Component {
                     />
 
                 )
+            
 
                 )}
 
--- employee-directory/src/Components/SearchBar/SearchBar.js
+++ employee-directory/src/Components/SearchBar/SearchBar.js
@@ -1,17 +1,29 @@
-import React  from 'react'
+import React from 'react'
 import "./SearchBar.css"
 
-export default function SearchBar (props) {
-   
-        return (
+export default function SearchBar(props) {
 
-            <section className="filter-container">
-                <form className="filter-form">
-                    <input onChange={props.handleSearch}></input>
-                </form>
-            </section>
+    return (
+
+        <section className="filter-container">
+            <form className="filter-form form-group">
+
+                <input onChange={props.handleSearch} type="text"></input>
+
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="male" />
+                    <label class="form-check-label" for="male">Check me out</label>
+                </div>
+
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="female" />
+                   <label class="form-check-label" for="female">Check me out</label>
+                </div>
+
+            </form>
+        </section>
 
 
-        )
-    }
+    )
+}
 
